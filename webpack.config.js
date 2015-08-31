c5c41899098ba40d744390320dbd3423e99a3040
Daiwei <daiweilu123@gmail.com>
2015-08-31 00:11:27 -0400

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './source/index.js',
+  devtool: 'source-map',
+  output: {
+    path: './public',
+    filename: 'index.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          optional: ['runtime', 'es7.objectRestSpread'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css!autoprefixer!sass?outputStyle=expanded'),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('index.css'),
+  ],
+
+};
